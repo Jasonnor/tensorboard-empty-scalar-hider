b113e68ed961fd2912102beae78d83748d84514a
Jasonnor <wujason810@gmail.com>
2019-08-09 01:53:42 +0800

    Check initialized and add displayAllScalar for action event

--- js/hider.js
+++ js/hider.js
@@ -1,4 +1,25 @@
+// Global variable initialized to ensure hider.js only run once
+if (typeof initialized === 'undefined') {
+  initialized = false
+}
+
 (function () {
+  if (initialized) {
+    return
+  }
+  initialized = true
+  console.info('TensorBoard Empty Scalar Hider: Successfully initialized.')
+  const actionList = document.querySelectorAll('.heading, .icon-container')
+  for (const action of actionList) {
+    action.addEventListener('click', () => {
+      displayAllScalar();
+      hideScalar();
+    })
+  }
+  hideScalar()
+})()
+
+function hideScalar() {
   const scalarList = document.getElementsByTagName('tf-scalar-card')
   let counter = 0
   for (const scalar of scalarList) {
@@ -11,4 +32,12 @@
     }
   }
   console.info(`TensorBoard Empty Scalar Hider: Successfully hide ${counter} elements.`)
-})();
+}
+
+function displayAllScalar() {
+  const scalarList = document.getElementsByTagName('tf-scalar-card')
+  for (const scalar of scalarList) {
+    scalar.style.display = 'block'
+  }
+  document.getElementById('reload-button').click()
+}
