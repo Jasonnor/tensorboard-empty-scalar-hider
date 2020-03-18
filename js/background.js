53f6c6b69ed17064b70965acd92955c6b99f2e43
Jasonnor <wujason810@gmail.com>
2020-03-18 12:12:20 +0800

    feature: hider on contextMenus

--- js/background.js
+++ js/background.js
@@ -3,3 +3,23 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     file: 'js/hider.js'
   });
 });
+
+chrome.contextMenus.create({
+  id: 'hideEmptyScalar',
+  title: 'Hide empty scalar',
+  contexts: ['page']
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  console.log('item ' + info.menuItemId + ' was clicked');
+  console.log('info: ' + JSON.stringify(info));
+  console.log('tab: ' + JSON.stringify(tab));
+  if (info.menuItemId == 'hideEmptyScalar') {
+    chrome.tabs.executeScript(null, {
+      file: 'js/hider.js'
+    });
+  }
+  if (chrome.extension.lastError) {
+    console.log("Got expected error: " + chrome.extension.lastError.message);
+  }
+});
